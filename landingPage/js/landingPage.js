62e22f09252163407cfdf79cdfc29709e172317c
Marcus Ciobanu <marcusciobanu11@gmail.com>
2023-11-14 15:16:33 +0000

    Renamed JS files and began some JS plus register.

---
+++ landingPage/js/landingPage.js
@@ -0,0 +1,5 @@
+// Defining constant to make writing easier
+
+const wrapper = document.querySelector('.wrapper')
+const loginLink = document.querySelector('.loginLink')
+const registerLink = document.querySelector('.registerLink')
