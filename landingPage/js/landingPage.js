3c997529fff2b0657b96dea5619ff06bd9c1c38c
Marcus Ciobanu <marcusciobanu11@gmail.com>
2023-11-15 12:12:14 +0000

    Finished login and register +  buttons all work.

--- landingPage/js/landingPage.js
+++ landingPage/js/landingPage.js
@@ -1,5 +1,31 @@
-// Defining constant to make writing easier
+// Defining constants to make writing easier and more concise
 
-const wrapper = document.querySelector('.wrapper')
-const loginLink = document.querySelector('.loginLink')
-const registerLink = document.querySelector('.registerLink')
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.loginLink');
+const registerLink = document.querySelector('.registerLink');
+const loginBtn = document.querySelector('.loginBtn');
+const closeButton = document.querySelector('.closeButton');
+
+// Detects when user clicks register button and adds class to .wrapper 
+
+registerLink.addEventListener('click', ()=> {
+  wrapper.classList.add('active');
+});
+
+// Detects when user clicks login button and removes class from .wrapper
+
+loginLink.addEventListener('click', ()=> {
+  wrapper.classList.remove('active');
+});
+
+// Detects when user clicks on menubar login button and adds popup class to .wrapper
+
+loginBtn.addEventListener('click', ()=> {
+  wrapper.classList.add('popup');
+});
+
+// Detects when user clicks on cross icon and removes popup class from .wrapper
+
+closeButton.addEventListener('click',()=> {
+  wrapper.classList.remove('popup');
+});
