c7e81ffa1c6f1832d43a731b4da50f1b53a875a7
Marcus Ciobanu <marcusciobanu11@gmail.com>
2024-02-07 08:38:32 +0000

    Redone landing page, added views

--- login_register/apps.py
+++ login_register/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LandingpageConfig(AppConfig):
+class LoginRegisterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'landingpage'
+    name = 'login_register'
