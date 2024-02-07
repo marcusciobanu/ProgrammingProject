15a8c5d6a4ac6b1c9e30da6e1f9c44bcc93fd590
Marcus Ciobanu <marcusciobanu11@gmail.com>
2024-02-07 09:30:30 +0000

    login routes to appropriate view and renders correctly

--- master/urls.py
+++ master/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('index.urls')),
-    path('login/', include('login_register.urls'))
+    path('login/', include('login.urls'))
 ]
--- static/index/js/index.js
+++ static/index/js/index.js
@@ -1,6 +1,6 @@
 // Defining constants to make writing easier and more concise
 
-const loginBtn = document.querySelector('.loginBtn');
+const loginBtn = document.querySelector(".loginBtn");
 
 // When loginBtn is clicked, it takes the user to the login page
 loginBtn.addEventListener('click', () => {
