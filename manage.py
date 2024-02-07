e6dd4e8870d0bb089d5a3a9cc95d143c07c8baa3
Marcus Ciobanu <marcusciobanu11@gmail.com>
2024-02-07 08:38:43 +0000

    Staged same changes as before

--- manage.py
+++ manage.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+#!/Users/marcusciobanu/.virtualenvs/europaFitness/bin/python
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'master.settings')
     try:
         from django.core.management import execute_from_command_line
--- static/js/index.js
+++ static/js/index.js
@@ -1,32 +1,8 @@
 // Defining constants to make writing easier and more concise
 
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.loginLink');
-const registerLink = document.querySelector('.registerLink');
 const loginBtn = document.querySelector('.loginBtn');
-const closeButton = document.querySelector('.closeButton');
 
-// Detects when user clicks register button and adds class to .wrapper 
-
-registerLink.addEventListener('click', ()=> {
-  wrapper.classList.add('active');
-});
-
-// Detects when user clicks login button and removes class from .wrapper
-
-loginLink.addEventListener('click', ()=> {
-  wrapper.classList.remove('active');
-});
-
-// Detects when user clicks on menubar login button and adds popup class to .wrapper
-
-loginBtn.addEventListener('click', ()=> {
-  wrapper.classList.add('popup');
-  wrapper.classList.remove('active')
-});
-
-// Detects when user clicks on cross icon and removes popup class from .wrapper
-
-closeButton.addEventListener('click',()=> {
-  wrapper.classList.remove('popup');
+// When loginBtn is clicked, it takes the user to the login page
+loginBtn.addEventListener('click', () => {
+  window.location.href = '/login';
 });
