c0dc11c7e2c4402d4a4cb95c5abf9fd35c0a7cf3
Marcus Ciobanu <marcusciobanu11@gmail.com>
2024-02-25 20:59:39 +0000

    + on previous commit

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,5 +6,6 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('create/', views.create, name='create')
+    path('create/', views.create, name='create'),
+    path('create/<int:exercise_id>/', views.delete_exercise, name='delete_exercise'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 from .forms import CreateExerciseForm
+from .models import Exercise, Workout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -12,12 +17,36 @@ def home(request):
 
 
 def create(request):
-    if request.method == 'POST':
-        form = CreateExerciseForm(request.POST, user=request.user)
-        if form.is_valid():
-            form.save()
+    if request.user.is_authenticated:
+        user_exercises = Exercise.objects.filter(user=request.user)
+        if request.method == 'POST':
+            form = CreateExerciseForm(request.POST, user=request.user)
+            if form.is_valid():
+                form.save()
+                form = CreateExerciseForm()
+            else:
+                form = CreateExerciseForm()
         else:
             form = CreateExerciseForm()
+        return render(request, 'exercisebank/create.html', {'form': form, 'user_exercises': user_exercises})
     else:
-        form = CreateExerciseForm()
-    return render(request, 'exercisebank/create.html', {'form': form})
+        return render(request, 'error/not-logged.html')
+
+
+@login_required
+def delete_exercise(request, exercise_id):
+    if request.method == 'POST':
+        exercise = Exercise.objects.get(id=exercise_id, user=request.user)
+        exercise.delete()
+        return HttpResponseRedirect(reverse('create'))
+    else:
+        return HttpResponseRedirect(reverse('create'))
+
+
+@login_required
+def start_workout(request):
+    new_workout = Workout(user=request.user, start_time=timezone.now())
+    new_workout.save()
+
+    return redirect('add_exercise', workout_id=new_workout.id)
+
